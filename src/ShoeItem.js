a42414ed72fe9445ee13b7c5087ed67d1852e3b3
Kyle Devine <devinenoise@gmail.com>
2020-02-21 11:01:17 -0800

    shoe styling

--- src/ShoeItem.js
+++ src/ShoeItem.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 export default class ShoeItem extends Component {
     render() {
         return (
-            <div>
+            <div class="shoe-box">
                 <li id="shoe-list">
                     <h1>{this.props.data.name}</h1>
-                    <h3>brand: {this.props.brand}</h3>
+                    <h3>brand: {this.props.data.brand}</h3>
                     <img class="shoe-images" src={this.props.data.url} alt={this.props.data.name} />
                     <h3>Type: {this.props.data.type}</h3>
-                    <h3>laces: {this.props.data.laces}</h3>
+                    <p>laces: {String(this.props.data.laces)}</p>
                 </li>
             </div>
         )
