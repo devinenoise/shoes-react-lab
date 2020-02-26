a6ec2cda251da84d38a6129cb7504507820a36f1
Kyle Devine <devinenoise@gmail.com>
2020-02-25 16:33:17 -0800

    Updates

---
+++ precache-manifest.e1f50070e39b2336fff0e34a13b9e4d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c284105baea901dc8ae63e6017b75ac5",
+    "url": "/shoes-react-lab/index.html"
+  },
+  {
+    "revision": "6cf86dcdea61fbb706f8",
+    "url": "/shoes-react-lab/static/css/main.c0b78cd6.chunk.css"
+  },
+  {
+    "revision": "84db013cd4a90b228db3",
+    "url": "/shoes-react-lab/static/js/2.ef148d18.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/shoes-react-lab/static/js/2.ef148d18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf86dcdea61fbb706f8",
+    "url": "/shoes-react-lab/static/js/main.add8ee84.chunk.js"
+  },
+  {
+    "revision": "72fd60d4118b015d8559",
+    "url": "/shoes-react-lab/static/js/runtime-main.6235c89a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shoes-react-lab/precache-manifest.96ace707bc36b9d6aca98af53a3b2c3e.js"
+  "/shoes-react-lab/precache-manifest.e1f50070e39b2336fff0e34a13b9e4d9.js"
 );
 
 self.addEventListener('message', (event) => {
